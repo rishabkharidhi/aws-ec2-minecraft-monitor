90ffbcc70c86e426cefa7ccd7292e8f50a0e7012
Rishab Kharidhi <47681612+rishabkharidhi@users.noreply.github.com>
2025-05-12 02:10:38 -0700

    Update deployment.sh

--- deployment.sh
+++ deployment.sh
@@ -5,65 +5,137 @@ rm -f stop
 rm -f start
 rm -f monitor
 
-#echo -e '#!/bin/bash\n# Variable which sets how long to wait until shutting down after the last player disconnects\n# Do not set this value under 5 minutes (300 seconds). A low value might result in the server shutting down before players can join.\nSSM_SHUTDOWNTIMER=$(aws ssm get-parameter --name "/mc/shutdowntimer" --query "Parameter.Value" --output text)\nDEFAULT_SHUTDOWNTIMER=900\nif [ $? -eq 0 ] && [[ "$SSM_SHUTDOWNTIMER" =~ ^[0-9]+$ ]] && [ "$SSM_SHUTDOWNTIMER" -gt 0 ]; then\nSHUTDOWNTIMER=$SSM_SHUTDOWNTIMER\nelse\nSHUTDOWNTIMER=$DEFAULT_SHUTDOWNTIMER\nfi\n\n# var to track time of last disconnect\nlastdisconnect=0\nwhile true; do\n# Get current connection\nconnections=$(netstat -antp | grep ":25565" | grep ESTABLISHED | wc -l)\nif [ $connections -eq 0 ]; then\necho "0 connections"\nif [ $lastdisconnect -eq 0 ]; then\necho "no players on server, starting timer..."\n# First disconnect, record time\nlastdisconnect=$(date +%s)\nelse\n# Check elapsed time since last disconnect\nnow=$(date +%s)\nelapsed=$((now - lastdisconnect))\necho "no players again, check if timer has reached time"\necho "shutdowntimer: $SHUTDOWNTIMER timer: $elapsed"\nif [ $elapsed -ge $SHUTDOWNTIMER ]; then\necho "timer has reached time, begin shutdown logic"\n# Over time, shutdown\n# Stop Minecraft server\necho "Stop Minecraft server"\nsudo ./stop\nsleep 20\n# Stop EC2 instance\n#echo "stop ec2 instance"\n#sudo shutdown -h now\nbreak\nfi\nfi\nelse\n# Connections exist, reset disconnect timer\necho "connections exist"\nlastdisconnect=0\nfi\n# Check again in 30 seconds\nsleep 30\ndone' >> monitor
-echo -e '#!/bin/bash\n\\n\SSM_SHUTDOWNTIMER=$(aws ssm get-parameter --name "/mc/shutdowntimer" --query "Parameter.Value" --output text)\n\DEFAULT_SHUTDOWNTIMER=900\n\if [ $? -eq 0 ] && [[ "$SSM_SHUTDOWNTIMER" =~ ^[0-9]+$ ]] && [ "$SSM_SHUTDOWNTIMER" -gt 0 ]; then\n\    SHUTDOWNTIMER=$SSM_SHUTDOWNTIMER\n\else\n\    SHUTDOWNTIMER=$DEFAULT_SHUTDOWNTIMER\n\fi\n\\n\MONITOR_IP="13.56.206.173"\n\MONITOR_PORT="25565"\n\echo "Monitoring connection and shutdown conditions for $MONITOR_IP:$MONITOR_PORT..."\n\\n\lastdisconnect=0\n\\n\while true; do\n\    mc_status=$(systemctl is-active minecraft.service)\n\\n\    if [ "$mc_status" != "active" ]; then\n\
-        connection=$(ss -tn state syn-recv "( sport = :$MONITOR_PORT )" | grep "$MONITOR_IP")\n\
-        if [[ -n "$connection" ]]; then\n\
-            echo "$(date): Connection requested to $MONITOR_IP:$MONITOR_PORT but server is off"\n\
-            echo "$(date): Starting Minecraft server"\n\
-            sudo systemctl start minecraft.service\n\
-        fi\n\
-    else\n\
-        connections=$(ss -tn state established "( sport = :$MONITOR_PORT )" | grep -c ESTAB)\n\
-        if [ "$connections" -eq 0 ]; then\n\
-            echo "0 connections"\n\
-            if [ "$lastdisconnect" -eq 0 ]; then\n\
-                echo "no players on server, starting timer..."\n\
-                lastdisconnect=$(date +%s)\n\
-            else\n\
-                now=$(date +%s)\n\
-                elapsed=$((now - lastdisconnect))\n\
-                echo "no players again, check if timer has reached time"\n\
-                echo "shutdowntimer: $SHUTDOWNTIMER timer: $elapsed"\n\
-                if [ "$elapsed" -ge "$SHUTDOWNTIMER" ]; then\n\
-                    echo "timer has reached time, begin shutdown logic"\n\
-                    echo "Stop Minecraft server"\n\
-                    sudo ./stop\n\
-                    sleep 20\n\
-                    break\n\
-                fi\n\
-            fi\n\
-        else\n\
-            echo "connections exist"\n\
-            lastdisconnect=0\n\
-        fi\n\
-    fi\n\
-\n\
-    sleep 2\n\
-done' >> monitor
+# Create monitor script
+cat <<EOF > monitor
+#!/bin/bash
+
+SSM_SHUTDOWNTIMER=$(aws ssm get-parameter --name "/mc/shutdowntimer" --query "Parameter.Value" --output text)
+DEFAULT_SHUTDOWNTIMER=900
+if [ $? -eq 0 ] && [[ "$SSM_SHUTDOWNTIMER" =~ ^[0-9]+$ ]] && [ "$SSM_SHUTDOWNTIMER" -gt 0 ]; then
+    SHUTDOWNTIMER=$SSM_SHUTDOWNTIMER
+else
+    SHUTDOWNTIMER=$DEFAULT_SHUTDOWNTIMER
+fi
+
+MONITOR_IP="13.56.206.173"
+MONITOR_PORT="25565"
+echo "Monitoring connection and shutdown conditions for \$MONITOR_IP:\$MONITOR_PORT..."
+
+lastdisconnect=0
+
+while true; do
+    mc_status=$(systemctl is-active minecraft.service)
+
+    if [ "\$mc_status" != "active" ]; then
+        connection=\$(ss -tn state syn-recv "( sport = :\$MONITOR_PORT )" | grep "\$MONITOR_IP")
+        if [[ -n "\$connection" ]]; then
+            echo "\$(date): Connection requested to \$MONITOR_IP:\$MONITOR_PORT but server is off"
+            echo "\$(date): Starting Minecraft server"
+            sudo systemctl start minecraft.service
+        fi
+    else
+        connections=\$(ss -tn state established "( sport = :\$MONITOR_PORT )" | grep -c ESTAB)
+        if [ "\$connections" -eq 0 ]; then
+            echo "0 connections"
+            if [ "\$lastdisconnect" -eq 0 ]; then
+                echo "no players on server, starting timer..."
+                lastdisconnect=\$(date +%s)
+            else
+                now=\$(date +%s)
+                elapsed=\$((now - lastdisconnect))
+                echo "no players again, check if timer has reached time"
+                echo "shutdowntimer: \$SHUTDOWNTIMER timer: \$elapsed"
+                if [ "\$elapsed" -ge "\$SHUTDOWNTIMER" ]; then
+                    echo "timer has reached time, begin shutdown logic"
+                    echo "Stop Minecraft server"
+                    sudo ./stop
+                    sleep 20
+                    break
+                fi
+            fi
+        else
+            echo "connections exist"
+            lastdisconnect=0
+        fi
+    fi
+
+    sleep 2
+done
+EOF
 
 chmod +x monitor
 
-# create server start file with SSM or default RAM
-echo -e '#!/bin/bash\nRAM=$(aws ssm get-parameter --name "/mc/ram" --query "Parameter.Value" --output text)\nDEFAULT_RAM="1300M"\necho "starting server!"\nif [ $? -eq 0 ] && [[ "$RAM" =~ ^[0-9]+$ ]] && [ "$RAM" -gt 0 ]; then\nram_size="${RAM}M"\nelse\nram_size="$DEFAULT_RAM"\nfi\necho "ram: $ram_size"\njava -Xmx$ram_size -Xms$ram_size -jar server.jar nogui' >> start
+# Create start script
+cat <<EOF > start
+#!/bin/bash
+RAM=\$(aws ssm get-parameter --name "/mc/ram" --query "Parameter.Value" --output text)
+DEFAULT_RAM="1300M"
+echo "starting server!"
+if [ \$? -eq 0 ] && [[ "\$RAM" =~ ^[0-9]+$ ]] && [ "\$RAM" -gt 0 ]; then
+    ram_size="\${RAM}M"
+else
+    ram_size="\$DEFAULT_RAM"
+fi
+echo "ram: \$ram_size"
+java -Xmx\$ram_size -Xms\$ram_size -jar server.jar nogui
+EOF
+
 chmod +x start
 
-echo -e '#!/bin/bash\necho "/stop" > /run/minecraft.stdin' >> stop
-chmod +x stop
+# Create stop script
+cat <<EOF > stop
+#!/bin/bash
+echo "/stop" > /run/minecraft.stdin
+EOF
 
+# Create systemd service files
 cd /etc/systemd/system/
-rm -f minecraft.service
-rm -f minecraft.socket
-rm -f monitor_minecraft.service
+rm -f minecraft.service minecraft.socket monitor_minecraft.service
 
-echo -e '[Unit]\nDescription=Minecraft Server on start up\n[Service]\nUser=minecraft\nWorkingDirectory=/opt/minecraft/server\nExecStart=/opt/minecraft/server/start\n \
-    Restart=on-failure\nSockets=minecraft.socket\nStandardInput=socket\nStandardOutput=journal\nStandardError=journal\n[Install]\nWantedBy=multi-user.target' >> minecraft.service
+cat <<EOF > minecraft.service
+[Unit]
+Description=Minecraft Server on start up
 
-echo -e '[Unit]\nPartOf=minecraft.service\n[Socket]\nListenFIFO=%t/minecraft.stdin' >> minecraft.socket
+[Service]
+User=minecraft
+WorkingDirectory=/opt/minecraft/server
+ExecStart=/opt/minecraft/server/start
+Restart=on-failure
+Sockets=minecraft.socket
+StandardInput=socket
+StandardOutput=journal
+StandardError=journal
 
-echo -e '[Unit]\nDescription=Monitor MC server for network connections\nWants=network-online.target\n[Service]\nUser=ec2-user\nWorkingDirectory=/opt/minecraft/server\n \
-    ExecStart=/opt/minecraft/server/monitor\nStandardInput=null\n[Install]\nWantedBy=multi-user.target' >> monitor_minecraft.service
+[Install]
+WantedBy=multi-user.target
+EOF
 
+cat <<EOF > minecraft.socket
+[Unit]
+PartOf=minecraft.service
+
+[Socket]
+ListenFIFO=%t/minecraft.stdin
+EOF
+
+cat <<EOF > monitor_minecraft.service
+[Unit]
+Description=Monitor MC server for network connections
+After=network-online.target
+Wants=network-online.target
+
+[Service]
+User=ec2-user
+WorkingDirectory=/opt/minecraft/server
+ExecStart=/opt/minecraft/server/monitor
+Restart=always
+StandardInput=null
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+sudo systemctl daemon-reexec
 sudo systemctl daemon-reload
 sudo systemctl enable minecraft.service
 sudo systemctl start minecraft.service
